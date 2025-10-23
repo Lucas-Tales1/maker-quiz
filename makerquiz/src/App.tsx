438777c38c6b2c38560e8d3383bfbeb17aacb9bb
Lucas-Tales1 <lucastales100@gmail.com>
2025-10-22 22:29:17 -0300

    feat: terminar atividade 1

--- makerquiz/src/App.tsx
+++ makerquiz/src/App.tsx
@@ -1,17 +1,10 @@
-import { Quiz } from './components/Quiz';
-import { Question } from './models/Question';
-
-const questions: Question[] = [
-  { id: 1, pergunta: 'Qual linguagem é usada no React?', opcoes: ['Python', 'JavaScript', 'Java'], respostaCorreta: 1 },
-  { id: 2, pergunta: 'PrimeReact é uma biblioteca para?', opcoes: ['Backend', 'UI', 'Banco de dados'], respostaCorreta: 1 },
-  { id: 3, pergunta: 'TypeScript é?', opcoes: ['Superset de JavaScript', 'Framework', 'Banco de dados'], respostaCorreta: 0 },
-];
+import { QuestionCrud } from './components/QuestionCrud';
 
 function App() {
   return (
     <div className="p-m-4">
-      <h1>MakerQuiz</h1>
-      <Quiz questions={questions} />
+      <h1>MakerQuiz - CRUD de Perguntas</h1>
+      <QuestionCrud />
     </div>
   );
 }
