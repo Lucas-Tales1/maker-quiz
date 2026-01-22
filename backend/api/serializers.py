c5f273abbac7f76a4891e85b2502c8e090374561
Lucas Tales <lucastales100@gmail.com>
2026-01-22 01:15:15 -0300

    refactor: refatorar serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,41 @@
 from rest_framework import serializers
 from .models import Question
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
         fields = '__all__'
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=6)
+    password2 = serializers.CharField(write_only=True, min_length=6)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2', 'email')
+
+    def validate(self, data):
+        if data['password'] != data.pop('password2'):
+            raise serializers.ValidationError('Passwords do not match.')
+        return data
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', '')
+        )
+        return user
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
